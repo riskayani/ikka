0e3ee1be459fcc24c05adafc5ff0dd99d59757fc
riskayani <78790207+riskayani@users.noreply.github.com>
2021-02-09 16:21:26 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>Genre Musik</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/logo/favicon.png" type="image/x-icon">
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="css/animate-3.7.0.css">
+    <link rel="stylesheet" href="css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="css/owl-carousel.min.css">
+    <link rel="stylesheet" href="css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- Preloader Starts -->
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Header Area Starts -->
+	<header class="header-area header-area2">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10">
+                    <div class="custom-navbar">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div> 
+                    <div class="main-menu main-menu2">
+                        <ul>
+                            <li class="active"><a href="index.php">HOME</a></li>
+                            <li><a href="about.php">GENRE MUSIK</a></li>
+                            <li><a href="menu.php">SEJARAH MUSIK</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+    <!-- Banner Area Starts -->
+    <section class="banner-area banner-area2 menu-bg text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1><i>GENRE MUSIK</i></h1>
+                   </div>
+            </div>
+        </div>
+    </section>
+<!-- Banner Area End -->
+
+<!-- Food Area starts -->
+<section class="food-area section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="section-top">
+                    <h3>BEBERAPA GENRE MUSIK</h3> 
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+    <!-- Welcome Area End -->
+    
+    <!-- Deshes Area Starts -->
+    <div class="deshes-area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-6 align-self-center">
+                    <div class="deshes-text">
+                        <h3>ROCK</h3>
+                        <p class="pt-3">Musik Rock adalah salah satu genre musik yang berasal dari perpaduan R&B, musik contry pada tahun 40-50an. Musik ini tergolong keras karena menggunakan ritme yang begitu berisik dengan alat musik yang banyak.</p>
+                    </div>
+                </div>
+                <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
+                    <img src="images/079987000_1456974986-20160303-Ilustrasi-lari-iStockphoto-1.jpg" alt="" class="img-fluid">
+                </div>
+            </div>
+                      <div class="row">
+                    <div class="col-lg-5 col-md-6 align-self-center">
+                        <div class="deshes-text">
+                            <h3>TRADISIONAL</h3>
+                            <p class="pt-3">Musik Tradional adalah seni budaya yang sejak lama turun temurun telah hidup dan berkembang didaerah tertentu. Musik ini tersebar hampir diseluruh pelosok negeri dan setiap daerahnya mempunyai ciri khas yang berbeda.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
+                        <img src="images/cycling-d12a43cbc32267e398e2857472bc43dd_600x400.jpg" alt="" class="img-fluid">
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-lg-5 col-md-6 align-self-center order-2 order-md-1 mt-4 mt-md-0">
+                        <img src="images/lompat-tali.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center order-1 order-md-2">
+                        <div class="deshes-text">
+                            <h3>JAZZ</h3>
+                            <p class="pt-3">Musik Jazz merupakan hasil penggabungan musik blues, ragtime, dan musik-musik band Eropa. Biasanya mmusik ini memiliki nada yang ringan dan mampu memberikan kesan yang rileks bagi para pendengarnya.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-lg-5 col-md-6 align-self-center order-2 order-md-1 mt-4 mt-md-0">
+                        <img src="images/1243888-manfaat-naik-turun-tangga.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-lg-5 offset-lg-2 col-md-6 align-self-center order-1 order-md-2">
+                        <div class="deshes-text">
+                            <h3>POP</h3>
+                            <p class="pt-3">Musik Pop merupakan jenis musik yang mempunyai nada dan lirik yang sederhana, mudah dipahami dan cocok untuk dinyanyikan atau disenandungkan. Musik ini bisa mengcakup segala bidang, baik percintaan, kemanusiaan, sosial dan lain sebagainya. Hal inilah yang menjadiakn musik pop sebagai musik universal.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    </div>
+
+
+    <!-- Javascript -->
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="js/vendor/bootstrap-4.1.3.min.js"></script>
+    <script src="js/vendor/wow.min.js"></script>
+    <script src="js/vendor/owl-carousel.min.js"></script>
+    <script src="js/vendor/jquery.datetimepicker.full.min.js"></script>
+    <script src="js/vendor/jquery.nice-select.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>Sejarah Musik</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/logo/favicon.png" type="image/x-icon">
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="css/animate-3.7.0.css">
+    <link rel="stylesheet" href="css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="css/owl-carousel.min.css">
+    <link rel="stylesheet" href="css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- Preloader Starts -->
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Header Area Starts -->
+	<header class="header-area header-area2">
+        <div class="container">
+            <div class="row">
+                 <div class="col-lg-10">
+                    <div class="custom-navbar">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div> 
+                    <div class="main-menu main-menu2">
+                    <ul>
+                            <li class="active"><a href="index.php">HOME</a></li>
+                            <li><a href="about.php">GENRE MUSIK</a></li>
+                            <li><a href="menu.php">SEJARAH MUSIK</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+    <!-- Banner Area Starts -->
+    <section class="banner-area banner-area2 menu-bg text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1><i>SEJARAH MUSIK</i></h1>
+                   </div>
+            </div>
+        </div>
+    </section>
+    <!-- Banner Area End -->
+
+    <!-- Food Area starts -->
+    <section class="food-area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="section-top">
+                        <h3>Sejarah Musik</h3> 
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-6">
+                    <div class="single-food">
+                        <div class="food-img">
+                            <img src="images/1242742-1000xauto-resep-makanan-sehat-untuk-diet.jpg" class="img-fluid" alt="">
+                        </div>
+                         <div class="customer-text">
+                         <img src="images/tielman0002.jpg" alt="" class="img-fluid">
+                         <p class="pt-3">Sejak abad ke-2 dan abad ke-3 sebelum Masehi, di Tiongkok dan Mesir ada musik yang mempunyai bentuk tertentu. Dengan mendapat pengaruh dari Mesir dan Babilon, berkembanglah musik Hibrani yang dikemudian hari berkembang menjadi musik Gereja. Musik itu kemudian disenangi oleh masyarakat, karena adanya pemain-pemain musik yang mengembara serta menyanyikan lagu yang dipakai pada upacara Gereja. Musik itu tersebar di seluruh Eropa kemudian tumbuh berkembang, dan musik instrumental maju dengan pesat. Setelah ada perbaikan pada alat-alat musik, misalnya biola dan cello. Kemudian timbulah alat musik Orgel. Komponis besar muncul di Jerman, Prancis, Italia, dan Rusia. Dalam abad ke 19, rasa kebangsaan mulai bangun dan berkembang. Oleh karena itu perkembangan musik pecah menurut kebangsaannya masing-masing, meskipun pada permulaannya sama-sama bergaya Romantik. Mulai abad 20, Prancis menjadi pelopor dengan musik Impresionistis yang segera diganti dengan musik Ekspresionistis.</p>
+                            
+                    
+                                                  
+    
+    </section>
+    <!-- Food Area End -->
+
+    
+
+    
+
+
+    <!-- Javascript -->
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="js/vendor/bootstrap-4.1.3.min.js"></script>
+    <script src="js/vendor/wow.min.js"></script>
+    <script src="js/vendor/owl-carousel.min.js"></script>
+    <script src="js/vendor/jquery.datetimepicker.full.min.js"></script>
+    <script src="js/vendor/jquery.nice-select.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
